9ee7dd98e424b58a7055086c4fc3047dcb8586c0
Shlok Shrikhande <130490530+shlok-shri@users.noreply.github.com>
2025-07-20 17:34:15 +0530

    Last

--- frontend/src/config/axios.js
+++ frontend/src/config/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 let axiosInstance = axios.create({
-    baseURL: import.meta.env.REACT_APP_API_URL,
+    baseURL: import.meta.env.VITE_API_URL,
 });
 
 // Always get the latest token from localStorage before each request
